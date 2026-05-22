4899f2f61ae93c7ef5404eeaffb8e723cb42e738
mrnazmul68 <mrnazmul68@gmail.com>
2026-05-22 17:38:52 +0600

    Move toaster notifications to top right

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Toaster position="top-center" reverseOrder={false} />
+    <Toaster position="top-right" reverseOrder={false} />
   </StrictMode>,
 )
